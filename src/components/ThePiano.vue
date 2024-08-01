b41c434d1852feedc4832bae55ccfc318adebdd1
nothehi <nodehi7813@gmail.com>
2024-08-01 09:32:47 +0330

    feat: add piano component

---
+++ src/components/ThePiano.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheOctave from './TheOctave.vue';
+
+</script>
+
+<template>
+  <div class="container flex flex-row w-full mx-auto h-full space-x-0.5">
+    <TheOctave :octave-number="4" />
+  </div>
+</template>
