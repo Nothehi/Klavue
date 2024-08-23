39d518914d932c201110632145506ba3bbfe7492
nothehi <nodehi7813@gmail.com>
2024-08-23 13:16:20 +0330

    feat: add another octabe to piano

--- src/components/ThePiano.vue
+++ src/components/ThePiano.vue
@@ -6,5 +6,6 @@ import TheOctave from './TheOctave.vue';
 <template>
   <div class="container flex flex-row w-full mx-auto h-full space-x-0.5">
     <TheOctave :number="4" />
+    <TheOctave :number="5" />
   </div>
 </template>
